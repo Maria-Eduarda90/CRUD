e7c0b2e60157e12bd6fb8ab9bc8f9d2463427967
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-27 13:00:19 -0300

    criação da interface pra criar o metodo de criar usuario, retorno e a tipagem do usuario

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -3,4 +3,16 @@ export interface User {
   name: string;
   email: string;
   password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface UserCreate {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface UserInterfaceRepository {
+  create(data: UserCreate): Promise<User>;
 }
