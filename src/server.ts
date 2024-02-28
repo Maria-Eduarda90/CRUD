f5eb89cb00c27cef07253cd26be7db7a4bd3bae2
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-28 14:24:12 -0300

    registrando a rota de contato no meu servidor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import fastify, { FastifyInstance } from "fastify";
 import { userRoutes } from "./routes/user.routes";
+import { contactsRoutes } from "./routes/contacts.routes";
 
 const app: FastifyInstance = fastify();
 
 app.register(userRoutes, {
   prefix: "/api/users",
+});
+
+app.register(contactsRoutes, {
+  prefix: "/api/contacts",
 });
 
 const port = 3333;
