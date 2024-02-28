00f1642626e015607fcf38055c572b29d85ee0e9
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-28 14:23:28 -0300

    criação da rota de login

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,18 +1,31 @@
 import { FastifyInstance } from "fastify";
 import { UserUseCase } from "../usecases/user.usecase";
-import { UserCreate } from "../interfaces/user.interface";
+import { UserCreate, UserLogin } from "../interfaces/user.interface";
 
 export async function userRoutes(fastify: FastifyInstance) {
-  const usecase = new UserUseCase();
+  const userCreated = new UserUseCase();
+  const userLogin = new UserUseCase();
   fastify.post<{ Body: UserCreate }>("/create", async (request, reply) => {
     const { name, email, password } = request.body;
     try {
-      const data = await usecase.create({
+      const data = await userCreated.create({
         name,
         email,
         password,
       });
       return reply.send(data);
+    } catch (error) {
+      reply.send(error);
+    }
+  });
+  fastify.post<{ Body: UserLogin }>("/login", async (request, reply) => {
+    const { email, password } = request.body;
+    try {
+      const data = await userLogin.login({
+        email,
+        password,
+      });
+      return reply.status(200).send(data);
     } catch (error) {
       reply.send(error);
     }
