be9da9efeda276a862a722e894141be56c34c299
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-03-05 13:23:55 -0300

    chore: camada onde fica o contato com o prisma

---
+++ src/repositories/contacts.repository.ts
@@ -0,0 +1,49 @@
+import { prisma } from "../prisma/config.prisma";
+import {
+  ContactsCreate,
+  Contacts,
+  ContactsInterfaceRepository,
+} from "./../interfaces/contacts.interface";
+export class ContactsRepository implements ContactsInterfaceRepository {
+  async create({
+    email,
+    name,
+    phone,
+    userId,
+  }: ContactsCreate): Promise<Contacts> {
+    try {
+      const createdContacts = await prisma.contacts.create({
+        data: {
+          email,
+          name,
+          phone,
+          userId,
+        },
+      });
+
+      return createdContacts;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async findByEmailOrPhone(
+    email: string,
+    phone: string
+  ): Promise<Contacts | null> {
+    const result = await prisma.contacts.findFirst({
+      where: {
+        OR: [
+          {
+            email,
+          },
+          {
+            phone,
+          },
+        ],
+      },
+    });
+
+    return result || null;
+  }
+}
