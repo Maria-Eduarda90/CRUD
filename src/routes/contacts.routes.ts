c44e5cf741ee1c880822e02b7d85cf395cabe923
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-03-05 13:24:37 -0300

    chore: criação da rota de contato

---
+++ src/routes/contacts.routes.ts
@@ -0,0 +1,32 @@
+import { FastifyInstance } from "fastify";
+import { ContactsUseCase } from "../usecases/contacts.usecase";
+import { ContactsCreate } from "../interfaces/contacts.interface";
+import { authMiddleware } from "../middlewares/auth.middleware";
+
+export async function contactsRoutes(fastify: FastifyInstance) {
+  const usecase = new ContactsUseCase();
+
+  try {
+    fastify.addHook("preHandler", authMiddleware);
+    fastify.post<{ Body: ContactsCreate; Params: { userId: string } }>(
+      "/create",
+      async (request, reply) => {
+        const { name, email, phone } = request.body;
+        const userId = request.params.userId;
+        try {
+          const data = await usecase.create({
+            name,
+            email,
+            phone,
+            userId: userId,
+          });
+          return reply.status(201).send(data);
+        } catch (err) {
+          reply.status(400).send();
+        }
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
